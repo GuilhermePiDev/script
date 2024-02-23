d3ed18c42333d82dfa104e17d8430fc56ea54f5d
GuilhermePiDev <guilhermepimenta976@gmail.com>
2024-02-22 21:55:09 -0300

    puxando img do form

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const endpoint = "https://pimenta.cognitiveservices.azure.com"
 const key = "0d9c4b7fb94c4e29b18aae5729dedd13"
 
 const container = document.getElementById("container")
-async function imagemAnalise(imageUrl) {
+const nomeImg = document.getElementById("nome")
+
+
+async function imagemAnalise() {
   fetch(`${endpoint}/vision/v3.1/analyze?visualFeatures=Tags&language=pt`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
       'Ocp-Apim-Subscription-Key': key
     },
-    body: JSON.stringify({ url: imageUrl })
+    body: JSON.stringify({ url: "https://armazenandohoje.blob.core.windows.net/contentorimg/"+nomeImg.value })
   })
   .then(response => {
     if (!response.ok) {
@@ -19,20 +22,19 @@ async function imagemAnalise(imageUrl) {
     return response.json();
   })
   .then(data => {
-    
+    const imgtag= document.createElement('div')
+    imgtag.innerHTML= `<img src="https://armazenandohoje.blob.core.windows.net/contentorimg/${nomeImg.value}" alt="">`
     const tagsElement = document.createElement('ul');
     tagsElement.innerHTML = data.tags.map(tag => `<li>${tag.name} (${tag.confidence.toFixed(2)})</li>`).join('');
-    container.appendChild(tagsElement);
+    imgtag.appendChild(tagsElement)
+    container.appendChild(imgtag);
+    
   })
-  .catch(error => {
    
-    console.error('Erro:', error);
-  });
-  
 }
 
- imagemAnalise("https://armazenandohoje.blob.core.windows.net/contentorimg/hisokinha.jpg")
- imagemAnalise("https://armazenandohoje.blob.core.windows.net/contentorimg/unomillinho.jpg")
+ 
+
 
 
 
